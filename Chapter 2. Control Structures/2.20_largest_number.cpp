b54a8b6965cafe9589c3fdcc3fcc37b17295fe12
Davit Kirakosyan <94119776+DavitK2005@users.noreply.github.com>
2026-05-20 23:06:10 +0400

    Add files via upload

---
+++ Chapter 2. Control Structures/2.20_largest_number.cpp
@@ -0,0 +1,28 @@
+/*The process of finding the largest number(i.e., the maximum of a group of numbers) is used
+frequently in computer applications.For example, a program that determines the winner of a sales
+contest would input the number of units sold by each salesperson.The salesperson who sells the most
+units wins the contest.Write a pseudocode program, then a C++ program that uses a while structure
+to determine and print the largest number of 10 numbers input by the user.Your program should use
+three variables, as follows :
+counter: A counter to count to 10 (i.e., to keep track of how many numbers have
+	been input and to determine when all 10 numbers have been processed).
+	number : The current number input to the program.
+	largest : The largest number found so far.*/
+
+#include <iostream>
+
+int main() {
+	int counter{1};
+	double number{};
+	double largest{};
+
+	while (counter <= 10) {
+		std::cout << "Enter a number: ";
+		std::cin >> number;
+		if (number > largest) {
+			largest = number;
+		}
+		counter++;
+	}
+	std::cout << largest;
+}
