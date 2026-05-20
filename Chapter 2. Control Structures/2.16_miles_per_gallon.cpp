ce0a17661cd5a8f37ebcc829011b6660ed00ac73
Davit Kirakosyan <94119776+DavitK2005@users.noreply.github.com>
2026-05-20 19:23:10 +0400

    Add files via upload

---
+++ Chapter 2. Control Structures/2.16_miles_per_gallon.cpp
@@ -0,0 +1,39 @@
+/*
+Drivers are concerned with the mileage obtained by their automobiles.One driver has kept
+track of several tankfuls of gasoline by recording miles driven and gallons used for each tankful.Develop a C++ program that uses a while structure to input the miles driven and gallons used for each
+tankful.The program should calculate and display the miles per gallon obtained for each tankful.After processing all input information, the program should calculate and print the combined miles per
+gallon obtained for all tankfuls.
+Enter the gallons used(-1 to end) : 12 .8
+Enter the mi les driven : 28*/
+
+#include <iostream>
+
+int main() {
+	double miles{};
+	double gallons{};
+	double overall_miles{};
+	double overall_gallons{};
+
+
+	while (gallons != -1) {
+		
+		std::cout << "Enter the gallons used (-1 to end)";
+		std::cin >> gallons;
+		if (gallons != -1)
+		{
+			overall_gallons += gallons;
+
+			std::cout << "Enter the miles driven";
+			std::cin >> miles;
+			overall_miles += miles;
+
+			if (gallons != 0) {
+				std::cout << miles / gallons << std::endl;
+			}
+			else {
+				std::cout << "Error";
+			}
+		}
+	}
+	std::cout << overall_miles / overall_gallons;
+}
