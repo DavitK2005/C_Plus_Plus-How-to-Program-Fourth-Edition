6cfe95d8b6fdb1cdb0b6d3682a58ef0f0d7ffc52
Davit Kirakosyan <94119776+DavitK2005@users.noreply.github.com>
2026-05-21 23:48:36 +0400

    Add files via upload

---
+++ Chapter 2. Control Structures/2.40_sum_sequence.cpp
@@ -0,0 +1,26 @@
+/*Write a program that uses a for structure to sum a sequence of integers.Assume that the
+first integer read specifies the number of values remaining to be entered.Your program should read
+only one value per input statement.A typical input sequence might be
+5 100 200 300 400 500
+where the 5 indicates that the subsequent 5 values are to be summed.*/
+
+#include <iostream>
+
+int main() {
+	int n{};
+
+	std::cout << "Enter a quantity of numers: ";
+	std::cin >> n;
+
+	int counter{};
+	int number{};
+	int sum{};
+	for (;counter < n;)
+	{
+		std::cout << "Enter a number: ";
+		std::cin >> number;
+		sum += number;
+		counter++;
+	}
+	std::cout << "The quantity of numbers is: " << n << std::endl << "The sum of sequence is: " << sum;
+}
