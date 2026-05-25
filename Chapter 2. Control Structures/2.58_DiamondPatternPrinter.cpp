70c4c8e6d5a659597c2bf25901f8bf472d9424fd
Davit Kirakosyan <94119776+DavitK2005@users.noreply.github.com>
2026-05-25 21:44:55 +0400

    Add files via upload

---
+++ Chapter 2. Control Structures/2.58_DiamondPatternPrinter.cpp
@@ -0,0 +1,32 @@
+/*Write a program that prints the following diamond shape.You may use output statements
+that print either a single asterisk(*) or a single blank.Maximize your use of repetition(with nested
+	for structures) and minimize the number of output statements.*/
+
+#include <iostream>
+
+int main() {
+	for (int row{ 1 }; row <= 5; row++) {
+		int spaces = 5 - row;
+		int stars = 2 * row - 1;
+
+		for (int i{ 0 }; i < spaces; i++) {
+			std::cout << ' ';
+		}
+		for (int j{}; j < stars; j++) {
+			std::cout << '*';
+		}
+		std::cout << std::endl;
+	}
+	for (int row{ 4 }; row >= 1; row--) {
+		int spaces = 5 - row;
+		int stars = 2 * row - 1;
+
+		for (int i{ 0 }; i < spaces; i++) {
+			std::cout << ' ';
+		}
+		for (int j{}; j < stars; j++) {
+			std::cout << '*';
+		}
+		std::cout << std::endl;
+	}
+}
